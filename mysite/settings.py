4a3630be3f2dd5c372bddaaac297dabdc3a97c57
Aditya-tops <ap7490944657@gmail.com>
2025-04-29 13:57:11 +0530

    auth

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"] #added by me
+LOGIN_REDIRECT_URL = 'food:index' #added by me
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
