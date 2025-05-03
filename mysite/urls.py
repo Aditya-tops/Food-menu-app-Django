d7b9d4b4cd4d8aaf6c7b30f822d44264dd342e73
Aditya-tops <ap7490944657@gmail.com>
2025-05-03 15:35:44 +0530

    .

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include,path
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+app_name = 'food'
 # from django.conf import settings
 # from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
