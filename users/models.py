fea27eeb0533c04f8792fd01c2fa76ee8702b94f
Aditya-tops <ap7490944657@gmail.com>
2025-05-04 23:10:27 +0530

    .

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_picture/',default='profilepic.jpg')
+    image = models.ImageField(upload_to='profile_picture/', default='defaultimage.jpg')
     location = models.CharField(max_length=100) # PIP INSTALL PILLOW for pic
 
     def __str__(self):
